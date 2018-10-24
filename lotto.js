2416133e38e062aac5bfd3c8c50e8619b215fb94
JA <joshappeldev@gmail.com>
2018-10-23 21:14:23 -0400

    first

---
+++ lotto.js
@@ -0,0 +1,30 @@
+$("#submit").on("click", function(event) {
+  event.preventDefault();
+
+
+let lotteryNumbers = [];
+
+let numberGenerator = function(arr) {
+ if (arr.length >= 5) return;
+ let newNumber = Math.floor(Math.random() * 75 + 1);
+ if (arr.indexOf(newNumber) < 0) {
+   arr.push(newNumber);
+ }
+ numberGenerator(arr);
+
+};
+
+numberGenerator(lotteryNumbers)
+lotteryNumbers.sort()
+
+let mega = Math.floor(Math.random() * 25 + 1);
+lotteryNumbers.push(mega)
+
+var row = $("<div>");
+row.addClass("numbers");
+
+row.append(`<p>Your numbers: ${lotteryNumbers}</p>`);
+
+$("#lotto-area").prepend(row);
+
+})
