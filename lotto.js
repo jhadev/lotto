05aea92f38da838ff7b446190c07405e3034fbc2
JA <joshappeldev@gmail.com>
2018-12-23 17:10:52 -0500

    added all NJ lottery games

--- lotto.js
+++ lotto.js
@@ -1,30 +1,149 @@
-$("#submit").on("click", function(event) {
+
+$("#pick3").on("click", event => {
+  event.preventDefault();
+  const pick3 = [];
+  const first = Math.floor(Math.random() * 9);
+  const second = Math.floor(Math.random() * 9);
+  const third = Math.floor(Math.random() * 9);
+
+  pick3.push(first, second, third);
+
+  const row = $("<div>");
+  row.addClass("numbers");
+
+  row.append(`<p>${pick3.join("")}</p>`);
+
+  $("#pick-3").prepend(row);
+});
+
+$("#pick4").on("click", event => {
+  event.preventDefault();
+  const pick4 = [];
+  const first = Math.floor(Math.random() * 9);
+  const second = Math.floor(Math.random() * 9);
+  const third = Math.floor(Math.random() * 9);
+  const fourth = Math.floor(Math.random() * 9);
+
+  pick4.push(first, second, third, fourth);
+
+  const row = $("<div>");
+  row.addClass("numbers");
+
+  row.append(`<p>${pick4.join("")}</p>`);
+
+  $("#pick-4").prepend(row);
+});
+
+$("#pick5").on("click", event => {
   event.preventDefault();
 
+  let lotteryNumbers = [];
 
-let lotteryNumbers = [];
+  let numberGenerator = arr => {
+    if (arr.length >= 5) return;
+    let newNumber = Math.floor(Math.random() * 43 + 1);
+    if (arr.indexOf(newNumber) < 0) {
+      arr.push(newNumber);
+    }
+    numberGenerator(arr);
+  };
 
-let numberGenerator = function(arr) {
- if (arr.length >= 5) return;
- let newNumber = Math.floor(Math.random() * 75 + 1);
- if (arr.indexOf(newNumber) < 0) {
-   arr.push(newNumber);
- }
- numberGenerator(arr);
+  numberGenerator(lotteryNumbers);
+  lotteryNumbers.sort(function (a, b) {
+    return a - b;
+  });
 
-};
+  let row = $("<div>");
+  row.addClass("numbers");
 
-numberGenerator(lotteryNumbers)
-lotteryNumbers.sort(function(a, b){return a - b})
+  row.append(`<p>${lotteryNumbers.join(", ")}</p>`);
 
-let mega = Math.floor(Math.random() * 25 + 1);
-lotteryNumbers.push(mega)
+  $("#pick-5").prepend(row);
+});
 
-var row = $("<div>");
-row.addClass("numbers");
+$("#pick6").on("click", event => {
+  event.preventDefault();
 
-row.append(`<p>Your numbers: ${lotteryNumbers}</p>`);
+  let lotteryNumbers = [];
 
-$("#lotto-area").prepend(row);
+  let numberGenerator = arr => {
+    if (arr.length >= 6) return;
+    let newNumber = Math.floor(Math.random() * 49 + 1);
+    if (arr.indexOf(newNumber) < 0) {
+      arr.push(newNumber);
+    }
+    numberGenerator(arr);
+  };
 
-})
+  numberGenerator(lotteryNumbers);
+  lotteryNumbers.sort(function (a, b) {
+    return a - b;
+  });
+
+  let row = $("<div>");
+  row.addClass("numbers");
+
+  row.append(`<p>${lotteryNumbers.join(", ")}</p>`);
+
+  $("#pick-6").prepend(row);
+});
+
+$("#mega").on("click", event => {
+  event.preventDefault();
+
+  let lotteryNumbers = [];
+
+  let numberGenerator = arr => {
+    if (arr.length >= 5) return;
+    let newNumber = Math.floor(Math.random() * 75 + 1);
+    if (arr.indexOf(newNumber) < 0) {
+      arr.push(newNumber);
+    }
+    numberGenerator(arr);
+  };
+
+  numberGenerator(lotteryNumbers);
+  lotteryNumbers.sort(function (a, b) {
+    return a - b;
+  });
+
+  let mega = Math.floor(Math.random() * 25 + 1);
+  lotteryNumbers.push(mega);
+
+  let row = $("<div>");
+  row.addClass("numbers");
+
+  row.append(`<p>${lotteryNumbers.join(", ")}</p>`);
+
+  $("#mega-area").prepend(row);
+});
+
+$("#power").on("click", event => {
+  event.preventDefault();
+
+  let lotteryNumbers = [];
+
+  let numberGenerator = arr => {
+    if (arr.length >= 5) return;
+    let newNumber = Math.floor(Math.random() * 69 + 1);
+    if (arr.indexOf(newNumber) < 0) {
+      arr.push(newNumber);
+    }
+    numberGenerator(arr);
+  };
+
+  numberGenerator(lotteryNumbers);
+  lotteryNumbers.sort(function (a, b) {
+    return a - b;
+  });
+
+  let mega = Math.floor(Math.random() * 26 + 1);
+  lotteryNumbers.push(mega);
+
+  let row = $("<div>");
+  row.addClass("numbers");
+
+  row.append(`<p>${lotteryNumbers.join(", ")}</p>`);
+
+  $("#powerball").prepend(row);
+});
